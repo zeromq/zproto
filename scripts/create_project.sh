57c074d4a95fa269eced561602ced7b8d416d10b
Martin Vala <mvala@saske.sk>
2014-07-03 12:46:30 +0400

    Added init script

---
+++ scripts/create_project.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+function help() {
+	echo "usage:"
+	echo "  $0 <project directory>"
+	echo
+	exit 1
+}
+
+[ $# -eq 1 ] || help
+if [ -d "$1" ];then
+	printf "\nError: Direcotry $1 already exists !!!\nPlease remove it first.\n\n"
+fi
+
+mkdir -p "$1"
+if [ ! -d "$1" ];then
+	printf "\nError: Direcotry $1 was not created !!!\n\n"
+	help
+fi
+
+PROJECT_DIR="$(dirname $(dirname $(readlink -m $0)))"
+MY_PROJ_NAME=$(basename "$1")
+
+cp -r $PROJECT_DIR/skeleton/* "$1"/ || exit 2
+cd "$1" || exit 3
+git init || exit 4
+
+find ./ -type f -exec sed -i -e 's/myproj/'$MY_PROJ_NAME'/g' {} \; || exti 5
+
+for f in $(find ./ -type f -name "*myproj*" -exec echo {} \;); do
+	mv $f ${f/myproj/$MY_PROJ_NAME} || exit 6
+done
+
+echo ""
+echo "###########################################################"
+echo "Project $MY_PROJ_NAME was creates successfully at $1"
+echo "###########################################################"
