e2fb11edab662eb5cb36889047cea164f8f4fd02
S R C <sroycode@gmail.com>
2014-07-22 21:06:41 +0530

    Update create_project.sh

--- scripts/create_project.sh
+++ scripts/create_project.sh
@@ -9,25 +9,27 @@ function help() {
 
 [ $# -eq 1 ] || help
 if [ -d "$1" ];then
-	printf "\nError: Direcotry $1 already exists !!!\nPlease remove it first.\n\n"
+	printf "\nError: Directory $1 already exists !!!\nPlease remove it first.\n\n"
     exit 1
 fi
 
 mkdir -p "$1"
 if [ ! -d "$1" ];then
-	printf "\nError: Direcotry $1 was not created !!!\n\n"
+	printf "\nError: Directory $1 was not created !!!\n\n"
 	help
 fi
 
-PROJECT_DIR="$(dirname $(dirname $(readlink -m $0)))"
+SCRIPTS_DIR=$(dirname $(cd ${0%/*} 2>>/dev/null ; echo `pwd`/${0##*/}))
+PROJECT_DIR="${SCRIPTS_DIR}/.."
 MY_PROJ_NAME=$(basename "$1")
+MY_PROJ_NAME_CAPS=`echo $MY_PROJ_NAME | tr '[a-z]' '[A-Z]'`
 
 cp -r $PROJECT_DIR/skeleton/* "$1"/ || exit 2
 cd "$1" || exit 3
 git init || exit 4
 
 find ./ -type f -exec sed -i -e 's/myproj/'$MY_PROJ_NAME'/g' {} \; || exit 5
-find ./ -type f -exec sed -i -e 's/MYPROJ/'${MY_PROJ_NAME^^}'/g' {} \; || exit 6
+find ./ -type f -exec sed -i -e 's/MYPROJ/'${MY_PROJ_NAME_CAPS}'/g' {} \; || exit 6
 
 for f in $(find ./ -type f -name "*myproj*" -exec echo {} \;); do
 	mv $f ${f/myproj/$MY_PROJ_NAME} || exit 7
