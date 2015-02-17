baf9bb2282a48b9a2117a9da992010e94ce8171d
Armen Baghumian <armen@OpenSourceClub.org>
2015-02-17 21:36:26 +0000

    Added multiline phpdoc support for long descriptions

--- src/php/Example/Types.php
+++ src/php/Example/Types.php
@@ -1,8 +1,8 @@
 <?php
 
 /**
- * Library Example is 100% generated. If you edit this file,
- * you will lose your changes at the next build cycle.
+ * Library Example is 100% generated. If you edit this file, you will
+ * lose your changes at the next build cycle.
  * DO NOT MAKE ANY CHANGES YOU WISH TO KEEP.
  *
  * The correct places for commits are:
