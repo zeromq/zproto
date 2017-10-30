3d47befa808624dfb01503682c785dd8c0589add
Doron Somech <somdoron@gmail.com>
2017-10-30 14:53:57 +0200

    problem: no fsharp support

---
+++ src/fsharp/ZProto.Tests/ExampleTests.fs
@@ -0,0 +1,203 @@
+module Example.Test
+
+open Xunit
+open FsUnit.Xunit
+open FsNetMQ
+open Example
+
+
+[<Fact>]
+let ``send and recv Log``() =
+    let msg = Log {
+        sequence = 123us;
+        level = 123uy;
+        event = 123uy;
+        node = 123us;
+        peer = 123us;
+        time = 123UL;
+        host = "Life is short but Now lasts for ever";
+        data = "Life is short but Now lasts for ever";
+    }
+
+    use server = Socket.dealer ()
+    Socket.bind server "inproc://Log.test"
+
+    use client = Socket.dealer ()
+    Socket.connect client "inproc://Log.test"
+
+    Example.send msg server
+
+    let msg' = Example.recv client
+
+    msg' |> should equal (Some msg)
+
+[<Fact>]
+let ``Log size fits stream ``() =
+    let log = {
+        sequence = 123us;
+        level = 123uy;
+        event = 123uy;
+        node = 123us;
+        peer = 123us;
+        time = 123UL;
+        host = "Life is short but Now lasts for ever";
+        data = "Life is short but Now lasts for ever";
+    }
+
+    let messageSize = Log.getMessageSize log
+
+    let stream =
+        Stream.create messageSize
+        |> Log.write log
+
+    let offset = Stream.getOffset stream
+
+    messageSize |> should equal offset
+
+[<Fact>]
+let ``send and recv Structures``() =
+    let msg = Structures {
+        sequence = 123us;
+        aliases = ["Name: Brutus";"Age: 43"];
+        headers =
+            Map.empty
+            |> Map.add "Name" "Brutus"
+            |> Map.add "Age" "43"
+    }
+
+    use server = Socket.dealer ()
+    Socket.bind server "inproc://Structures.test"
+
+    use client = Socket.dealer ()
+    Socket.connect client "inproc://Structures.test"
+
+    Example.send msg server
+
+    let msg' = Example.recv client
+
+    msg' |> should equal (Some msg)
+
+[<Fact>]
+let ``Structures size fits stream ``() =
+    let structures = {
+        sequence = 123us;
+        aliases = ["Name: Brutus";"Age: 43"];
+        headers =
+            Map.empty
+            |> Map.add "Name" "Brutus"
+            |> Map.add "Age" "43"
+    }
+
+    let messageSize = Structures.getMessageSize structures
+
+    let stream =
+        Stream.create messageSize
+        |> Structures.write structures
+
+    let offset = Stream.getOffset stream
+
+    messageSize |> should equal offset
+
+[<Fact>]
+let ``send and recv Binary``() =
+    let msg = Binary {
+        sequence = 123us;
+        flags = Array.create 4 123uy;
+        publicKey = "Captcha Diem"B;
+        identifier = System.Guid.Parse("DED9D22A-FCE1-488B-8761-84752ACC113E");
+    }
+
+    use server = Socket.dealer ()
+    Socket.bind server "inproc://Binary.test"
+
+    use client = Socket.dealer ()
+    Socket.connect client "inproc://Binary.test"
+
+    Example.send msg server
+
+    let msg' = Example.recv client
+
+    msg' |> should equal (Some msg)
+
+[<Fact>]
+let ``Binary size fits stream ``() =
+    let binary = {
+        sequence = 123us;
+        flags = Array.create 4 123uy;
+        publicKey = "Captcha Diem"B;
+        identifier = System.Guid.Parse("DED9D22A-FCE1-488B-8761-84752ACC113E");
+    }
+
+    let messageSize = Binary.getMessageSize binary
+
+    let stream =
+        Stream.create messageSize
+        |> Binary.write binary
+
+    let offset = Stream.getOffset stream
+
+    messageSize |> should equal offset
+
+[<Fact>]
+let ``send and recv Types``() =
+    let msg = Types {
+        sequence = 123us;
+        clientForename = "Life is short but Now lasts for ever";
+        clientSurname = "Life is short but Now lasts for ever";
+        clientMobile = "Life is short but Now lasts for ever";
+        clientEmail = "Life is short but Now lasts for ever";
+        supplierForename = "Life is short but Now lasts for ever";
+        supplierSurname = "Life is short but Now lasts for ever";
+        supplierMobile = "Life is short but Now lasts for ever";
+        supplierEmail = "Life is short but Now lasts for ever";
+    }
+
+    use server = Socket.dealer ()
+    Socket.bind server "inproc://Types.test"
+
+    use client = Socket.dealer ()
+    Socket.connect client "inproc://Types.test"
+
+    Example.send msg server
+
+    let msg' = Example.recv client
+
+    msg' |> should equal (Some msg)
+
+[<Fact>]
+let ``Types size fits stream ``() =
+    let types = {
+        sequence = 123us;
+        clientForename = "Life is short but Now lasts for ever";
+        clientSurname = "Life is short but Now lasts for ever";
+        clientMobile = "Life is short but Now lasts for ever";
+        clientEmail = "Life is short but Now lasts for ever";
+        supplierForename = "Life is short but Now lasts for ever";
+        supplierSurname = "Life is short but Now lasts for ever";
+        supplierMobile = "Life is short but Now lasts for ever";
+        supplierEmail = "Life is short but Now lasts for ever";
+    }
+
+    let messageSize = Types.getMessageSize types
+
+    let stream =
+        Stream.create messageSize
+        |> Types.write types
+
+    let offset = Stream.getOffset stream
+
+    messageSize |> should equal offset
+
+[<Fact>]
+let ``malformed message return None``() =
+    use server = Socket.dealer ()
+    Socket.bind server "inproc://Example.test"
+
+    use client = Socket.dealer ()
+    Socket.connect client "inproc://Example.test"
+
+    Frame.send server "hello world"B
+
+    let msg = Example.recv client
+    msg |> should equal None
+ 
